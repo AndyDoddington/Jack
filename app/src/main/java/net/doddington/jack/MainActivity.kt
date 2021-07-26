911493fb434838ead14652c3be653461feaf495a
Andy Doddington <andy@kndj.co.uk>
2021-07-26 23:28:02 +0100

    Need to fix one (or two?) remaining errors.

--- app/src/main/java/net/doddington/jack/MainActivity.kt
+++ app/src/main/java/net/doddington/jack/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
         locationPermissionHelper = LocationPermissionHelper(this)
         locationPermissionHelper.checkPermissions {
             mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) { _ ->
